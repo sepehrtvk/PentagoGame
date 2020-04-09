6fd745fabb76394d11234f0becd88e9f7eb8fb47
9831111 <9831111@ceit.local>
2020-04-09 12:58:47 +0430

    addNut method

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,173 @@
 public class Board extends PentagonGame {
+    public void addNut(int boardNumber, int blockNumber) {
+
+        switch (boardNumber) {
+            case 1:
+                switch (blockNumber) {
+                    case 1:
+                        if (map[0][0].equals("⚪"))
+                            map[0][0] = nutColor;
+                        break;
+                    case 2:
+                        if (map[0][1].equals("⚪"))
+                            map[0][1] = nutColor;
+                        break;
+                    case 3:
+                        if (map[0][2].equals("⚪"))
+                            map[0][2] = nutColor;
+                        break;
+                    case 4:
+                        if (map[1][0].equals("⚪"))
+                            map[1][0] = nutColor;
+                        break;
+                    case 5:
+                        if (map[1][1].equals("⚪"))
+                            map[1][1] = nutColor;
+                        break;
+                    case 6:
+                        if (map[1][2].equals("⚪"))
+                            map[1][2] = nutColor;
+                        break;
+                    case 7:
+                        if (map[2][0].equals("⚪"))
+                            map[2][0] = nutColor;
+                        break;
+                    case 8:
+                        if (map[2][1].equals("⚪"))
+                            map[2][1] = nutColor;
+                        break;
+                    case 9:
+                        if (map[2][2].equals("⚪"))
+                            map[2][2] = nutColor;
+                        break;
+
+                }
+                break;
+            case 2:
+                switch (blockNumber) {
+                    case 1:
+                        if (map[0][3].equals("⚪"))
+                            map[0][3] = nutColor;
+                        break;
+                    case 2:
+                        if (map[0][4].equals("⚪"))
+                            map[0][4] = nutColor;
+                        break;
+                    case 3:
+                        if (map[0][5].equals("⚪"))
+                            map[0][5] = nutColor;
+                        break;
+                    case 4:
+                        if (map[1][3].equals("⚪"))
+                            map[1][3] = nutColor;
+                        break;
+                    case 5:
+                        if (map[1][4].equals("⚪"))
+                            map[1][4] = nutColor;
+                        break;
+                    case 6:
+                        if (map[1][5].equals("⚪"))
+                            map[1][5] = nutColor;
+                        break;
+                    case 7:
+                        if (map[2][3].equals("⚪"))
+                            map[2][3] = nutColor;
+                        break;
+                    case 8:
+                        if (map[2][4].equals("⚪"))
+                            map[2][4] = nutColor;
+                        break;
+                    case 9:
+                        if (map[2][5].equals("⚪"))
+                            map[2][5] = nutColor;
+                        break;
+
+                }
+                break;
+            case 3:
+                switch (blockNumber) {
+                    case 1:
+                        if (map[3][0].equals("⚪"))
+                            map[3][0] = nutColor;
+                        break;
+                    case 2:
+                        if (map[3][1].equals("⚪"))
+                            map[3][1] = nutColor;
+                        break;
+                    case 3:
+                        if (map[3][2].equals("⚪"))
+                            map[3][2] = nutColor;
+                        break;
+                    case 4:
+                        if (map[4][0].equals("⚪"))
+                            map[4][0] = nutColor;
+                        break;
+                    case 5:
+                        if (map[4][1].equals("⚪"))
+                            map[4][1] = nutColor;
+                        break;
+                    case 6:
+                        if (map[4][2].equals("⚪"))
+                            map[4][2] = nutColor;
+                        break;
+                    case 7:
+                        if (map[5][0].equals("⚪"))
+                            map[5][0] = nutColor;
+                        break;
+                    case 8:
+                        if (map[5][1].equals("⚪"))
+                            map[5][1] = nutColor;
+                        break;
+                    case 9:
+                        if (map[5][2].equals("⚪"))
+                            map[5][2] = nutColor;
+                        break;
+
+                }
+                break;
+            case 4:
+                switch (blockNumber) {
+                    case 1:
+                        if (map[3][3].equals("⚪"))
+                            map[3][3] = nutColor;
+                        break;
+                    case 2:
+                        if (map[3][4].equals("⚪"))
+                            map[3][4] = nutColor;
+                        break;
+                    case 3:
+                        if (map[3][5].equals("⚪"))
+                            map[3][5] = nutColor;
+                        break;
+                    case 4:
+                        if (map[4][3].equals("⚪"))
+                            map[4][3] = nutColor;
+                        break;
+                    case 5:
+                        if (map[4][4].equals("⚪"))
+                            map[4][4] = nutColor;
+                        break;
+                    case 6:
+                        if (map[4][5].equals("⚪"))
+                            map[4][5] = nutColor;
+                        break;
+                    case 7:
+                        if (map[5][3].equals("⚪"))
+                            map[5][3] = nutColor;
+                        break;
+                    case 8:
+                        if (map[5][4].equals("⚪"))
+                            map[5][4] = nutColor;
+                        break;
+                    case 9:
+                        if (map[5][5].equals("⚪"))
+                            map[5][5] = nutColor;
+                        break;
+
+                }
+                break;
+        }
+        checkEndGame();
+    }
+
 }
